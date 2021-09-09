60da97351b55c38aff0cc2722a10f56c130f26cf
CARTERON Paul <carteronpaul@gmail.com>
2021-09-09 14:19:16 +0200

    New import_stand_data function

---
+++ R/import_stand_data.R
@@ -0,0 +1,49 @@
+#' import_stand_data
+#'
+#' @param filepath filepath of the output folder from SimCop. You need at least "stand_data.csv" and "parameter_scenarii.csv"
+#' @param after_mortality_only if TRUE, the result only keep values after mortality
+#'
+#' @return data.frame of stand informations
+#' @export
+#'
+#' @importFrom vroom vroom
+#' @importFrom dplyr group_by mutate ungroup left_join rename filter
+#' @importFrom purrr map_df
+#'
+import_stand_data = function(filepath, after_mortality_only = TRUE){
+
+   Nha <- Vha <- Vha_dead <- douglas.hDom50 <- horStemSpacingM <- parameters_id <- repetitions <- NULL
+   fertility <- density <- stand_age <- NULL
+
+   rawdata = vroom(file = file.path(filepath,"stand_data.csv"),
+                   col_select = base::c("parameters_id","repetitions","rotations","stand_age",
+                                        "Nha","Gha","Vha","Hdom","Hg","Cdom","Cg","Vha_dead","Dg","Ddom",
+                                        "Total_crown_cover_ha","Out_of_cover_crown_cover_ha"),
+                   col_types = c(.default = "d")) %>%
+      group_by(parameters_id,repetitions) %>%
+      mutate(Vha_dead = cumsum(Vha_dead)) %>%
+      ungroup() %>%
+      mutate(Vha_mean = Vha/Nha,
+             Vha_Tot = Vha + Vha_dead)
+
+   parameters = vroom(file = file.path(filepath,"parameter_scenarii.csv"),
+                      col_select = base::c("parameters_id","plotDimXM","horStemSpacingM",
+                                           "douglas.hDom50"),
+                      col_types = c(.default = "d")) %>%
+      mutate(density = as.integer(10000/horStemSpacingM^2))
+
+   stand_data = left_join(rawdata,parameters, by = "parameters_id") %>%
+      map_df(~ base::as.double(.)) %>%
+      rename(fertility = douglas.hDom50)
+
+   if (after_mortality_only == TRUE){
+      stand_data_2 = stand_data %>%
+         group_by(repetitions,fertility,density) %>%
+         filter(!duplicated(stand_age, fromLast = TRUE))
+   }
+
+   return(stand_data)
+}
+
+
+filepath = "D:/Professionnel/INRA Apprentissage/Projet/2021-04-27 - SDMD/input/avec_contrainte_2"
